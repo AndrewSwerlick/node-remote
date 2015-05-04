2c426b60c965f20ef3e364ed137c2fbb77572ef4
Liam Cottam <killtheidols@hotmail.co.uk>
2015-05-04 16:16:57 +0100

    Remove nodules

--- node_modules/x11/lib/x11/index.js
+++
@@ -1,7 +0,0 @@
-var core = require('./xcore');
-var em = require('./eventmask').eventMask;
-var keysyms = require('./keysyms');
-
-module.exports.createClient = core.createClient;
-module.exports.eventMask = em;
-module.exports.keySyms = keysyms;
--- node_modules/x11/test/pixmap.js
+++
@@ -1,9 +0,0 @@
-function Pixmap(depth, width, height, buffer)
-{
-    this.depth = depth;
-    this.width = width;
-    this.height = height;
-    this.data = buffer;    
-}
-
-module.exports.Pixmap = Pixmap;
